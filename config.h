8acb70ba855f860293278cb2467e0248e46ce7ff
Muhamad Sammar <muhamad.sammar@gmail.com>
2018-11-23 20:47:36 +0200

    new clean xapian

---
+++ config.h
@@ -0,0 +1,528 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* directory separator(s) as a character or string literal */
+#define DIR_SEPS '/'
+
+/* directory separator(s) as an initialiser list */
+#define DIR_SEPS_LIST { '/' }
+
+/* Define to use flock() for flint-compatible locking */
+/* #undef FLINTLOCK_USE_FLOCK */
+
+/* Define if ftime returns void */
+/* #undef FTIME_RETURNS_VOID */
+
+/* Define to 1 if you have the `clock_gettime' function. */
+#define HAVE_CLOCK_GETTIME 1
+
+/* Define to 1 if you have the `closefrom' function. */
+/* #undef HAVE_CLOSEFROM */
+
+/* define if the compiler supports basic C++11 syntax */
+/* #undef HAVE_CXX11 */
+
+/* Define to 1 if you have the <cxxabi.h> header file. */
+#define HAVE_CXXABI_H 1
+
+/* Define to 1 if you have the declaration of `exp10(double)', and to 0 if you
+   don't. */
+#define HAVE_DECL_EXP10 1
+
+/* Define to 1 if you have the declaration of `log2(double)', and to 0 if you
+   don't. */
+#define HAVE_DECL_LOG2 1
+
+/* Define to 1 if you have the declaration of `strerror_r', and to 0 if you
+   don't. */
+#define HAVE_DECL_STRERROR_R 1
+
+/* Define to 1 if you have the declaration of `_byteswap_ulong', and to 0 if
+   you don't. */
+#define HAVE_DECL__BYTESWAP_ULONG 0
+
+/* Define to 1 if you have the declaration of `_byteswap_ushort', and to 0 if
+   you don't. */
+#define HAVE_DECL__BYTESWAP_USHORT 0
+
+/* Define to 1 if you have the declaration of `_putenv_s(const char *,const
+   char *)', and to 0 if you don't. */
+#define HAVE_DECL__PUTENV_S 0
+
+/* Define to 1 if you have the declaration of `__builtin_add_overflow(int,
+   int, int*)', and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_ADD_OVERFLOW 1
+
+/* Define to 1 if you have the declaration of `__builtin_bswap16(uint16_t)',
+   and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_BSWAP16 1
+
+/* Define to 1 if you have the declaration of `__builtin_bswap32(uint32_t)',
+   and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_BSWAP32 1
+
+/* Define to 1 if you have the declaration of `__builtin_clz(unsigned)', and
+   to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_CLZ 1
+
+/* Define to 1 if you have the declaration of `__builtin_clzl(unsigned long)',
+   and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_CLZL 1
+
+/* Define to 1 if you have the declaration of `__builtin_clzll(unsigned long
+   long)', and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_CLZLL 1
+
+/* Define to 1 if you have the declaration of `__builtin_ctz(unsigned)', and
+   to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_CTZ 1
+
+/* Define to 1 if you have the declaration of `__builtin_ctzl(unsigned long)',
+   and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_CTZL 1
+
+/* Define to 1 if you have the declaration of `__builtin_ctzll(unsigned long
+   long)', and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_CTZLL 1
+
+/* Define to 1 if you have the declaration of `__builtin_expect(long, long)',
+   and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_EXPECT 1
+
+/* Define to 1 if you have the declaration of `__builtin_mul_overflow(int,
+   int, int*)', and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_MUL_OVERFLOW 1
+
+/* Define to 1 if you have the declaration of `__builtin_popcount(unsigned)',
+   and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_POPCOUNT 1
+
+/* Define to 1 if you have the declaration of `__builtin_popcountl(unsigned
+   long)', and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_POPCOUNTL 1
+
+/* Define to 1 if you have the declaration of `__builtin_popcountll(unsigned
+   long long)', and to 0 if you don't. */
+#define HAVE_DECL___BUILTIN_POPCOUNTLL 1
+
+/* Define to 1 if you have the declaration of `__exp10(double)', and to 0 if
+   you don't. */
+#define HAVE_DECL___EXP10 1
+
+/* Define to 1 if you have the declaration of `__popcnt', and to 0 if you
+   don't. */
+#define HAVE_DECL___POPCNT 0
+
+/* Define to 1 if you have the declaration of `__popcnt64', and to 0 if you
+   don't. */
+#define HAVE_DECL___POPCNT64 0
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `fdatasync' function. */
+#define HAVE_FDATASYNC 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the `fsync' function. */
+#define HAVE_FSYNC 1
+
+/* Define to 1 if you have the `ftime' function. */
+#define HAVE_FTIME 1
+
+/* Define to 1 if you have the `ftruncate' function. */
+#define HAVE_FTRUNCATE 1
+
+/* Define to 1 if you have the `getdirentries' function. */
+#define HAVE_GETDIRENTRIES 1
+
+/* Define to 1 if you have the `gethostname' function. */
+#define HAVE_GETHOSTNAME 1
+
+/* Define to 1 if you have the `getrlimit' function. */
+#define HAVE_GETRLIMIT 1
+
+/* Define to 1 if you have the `getrusage' function. */
+#define HAVE_GETRUSAGE 1
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <limits.h> header file. */
+#define HAVE_LIMITS_H 1
+
+/* Define to 1 if you have the `link' function. */
+#define HAVE_LINK 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `nanosleep' function. */
+#define HAVE_NANOSLEEP 1
+
+/* Define to 1 if you have the `nftw' function. */
+#define HAVE_NFTW 1
+
+/* Define to 1 if you have the `posix_fadvise' function. */
+#define HAVE_POSIX_FADVISE 1
+
+/* Define if pread is available on this system */
+#define HAVE_PREAD 1
+
+/* Define if pwrite is available on this system */
+#define HAVE_PWRITE 1
+
+/* Define to 1 if you have the `random' function. */
+#define HAVE_RANDOM 1
+
+/* Define to 1 if you have the `setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define to 1 if you have the `sigaction' function. */
+#define HAVE_SIGACTION 1
+
+/* Define to 1 if you have the 'sigsetjmp' function */
+#define HAVE_SIGSETJMP 1
+
+/* Define to 1 if you have the `sleep' function. */
+#define HAVE_SLEEP 1
+
+/* Define to 1 if you have the 'socketpair' function. */
+#define HAVE_SOCKETPAIR 1
+
+/* Define to 1 if you have the `srandom' function. */
+#define HAVE_SRANDOM 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strerror_r' function. */
+#define HAVE_STRERROR_R 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `sysconf' function. */
+#define HAVE_SYSCONF 1
+
+/* Define if you have 'sys_errlist' and 'sys_nerr' */
+#define HAVE_SYS_ERRLIST_AND_SYS_NERR 1
+
+/* Define to 1 if you have the <sys/errno.h> header file. */
+#define HAVE_SYS_ERRNO_H 1
+
+/* Define to 1 if you have the <sys/resource.h> header file. */
+#define HAVE_SYS_RESOURCE_H 1
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/utsname.h> header file. */
+#define HAVE_SYS_UTSNAME_H 1
+
+/* Define to 1 if you have the 'timer_create' function. */
+#define HAVE_TIMER_CREATE 1
+
+/* Define to 1 if you have the `times' function. */
+#define HAVE_TIMES 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the <uuid.h> header file. */
+/* #undef HAVE_UUID_H */
+
+/* Define to 1 if you have the <uuid/uuid.h> header file. */
+/* #undef HAVE_UUID_UUID_H */
+
+/* Define if a suitable valgrind is installed */
+/* #undef HAVE_VALGRIND */
+
+/* Define to 1 if you have the <valgrind/memcheck.h> header file. */
+/* #undef HAVE_VALGRIND_MEMCHECK_H */
+
+/* Define to 1 if you have the <zlib.h> header file. */
+#define HAVE_ZLIB_H 1
+
+/* Define if you have '_sys_errlist' and '_sys_nerr' */
+#define HAVE__SYS_ERRLIST_AND__SYS_NERR 1
+
+/* Define to 1 if you have the '__builtin_exp10' function. */
+#define HAVE___BUILTIN_EXP10 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Define on mingw to get _s suffixed "secure" functions declared in headers
+   */
+/* #undef MINGW_HAS_SECURE_API */
+
+/* Name of package */
+#define PACKAGE "xapian-core"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "https://xapian.org/bugs"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "xapian-core"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "xapian-core 1.4.9"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "xapian-core"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.4.9"
+
+/* explicit prototype needed for pread (if any) */
+/* #undef PREAD_PROTOTYPE */
+
+/* explicit prototype needed for pwrite (if any) */
+/* #undef PWRITE_PROTOTYPE */
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of `long', as computed by sizeof. */
+#define SIZEOF_LONG 8
+
+/* The size of `long long', as computed by sizeof. */
+#define SIZEOF_LONG_LONG 8
+
+/* The size of `short', as computed by sizeof. */
+#define SIZEOF_SHORT 2
+
+/* Define to the name of a function implementing snprintf but not caring about
+   ISO C99 return value semantics (if one exists) */
+#define SNPRINTF snprintf
+
+/* Define to the name of a function implementing snprintf with ISO C99
+   semantics (if one exists) */
+#define SNPRINTF_ISO snprintf
+
+/* type to use for 5th parameter to getsockopt */
+#define SOCKLEN_T socklen_t
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if strerror_r returns char *. */
+#define STRERROR_R_CHAR_P 1
+
+/* Define to 1 to read UUID from '/proc/sys/kernel/random/uuid' */
+#define USE_PROC_FOR_UUID 1
+
+/* Define if the testsuite can use RTTI */
+#define USE_RTTI 1
+
+/* Define to 1 to use UuidCreate(), etc */
+/* #undef USE_WIN32_UUID_API */
+
+/* Version number of package */
+#define VERSION "1.4.9"
+
+/* Version of Windows to assume (0x600 => Vista). */
+/* #undef WINVER */
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+/* Define if you want assertions (causes some slow-down) */
+/* #undef XAPIAN_ASSERTIONS */
+
+/* Define if you want paranoid assertions (causes significant slow-down) */
+/* #undef XAPIAN_ASSERTIONS_PARANOID */
+
+/* Define if you want a log of methods called and other debug messages */
+/* #undef XAPIAN_DEBUG_LOG */
+
+/* Enable large inode numbers on Mac OS X 10.5.  */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
+
+/* Number of bits in a file offset, on hosts where this is settable. */
+/* #undef _FILE_OFFSET_BITS */
+
+/* Define for large files, on AIX-style hosts. */
+/* #undef _LARGE_FILES */
+
+/* Version of Windows to assume. */
+/* #undef _WIN32_WINNT */
+
+/* Define on mingw to the minimum msvcrt version to assume */
+/* #undef __MSVCRT_VERSION__ */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef mode_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef pid_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef ssize_t */
+
+/* Disable stupid MSVC warnings. */
+#ifdef _MSC_VER
+/* Passing an empty parameter to a single parameter macro. */
+# pragma warning(disable:4003)
+/* A "performance" warning for converting int to bool. */
+# pragma warning(disable:4800)
+
+/* Tell MSVC we want M_PI, etc defined. */
+# define _USE_MATH_DEFINES
+
+/* Tell MSVC we don't want max() and min() macros defined. */
+# define NOMINMAX
+#endif
+
+/* Make the POSIX-like functions support large files.  MSVC needs this;
+ * current mingw32 does too; mingw64 supports _FILE_OFFSET_BITS, which
+ * AC_SYS_LARGEFILE should discover and enable automatically.
+ */
+#if defined _MSC_VER || \
+    (defined __MINGW32__ && !defined _FILE_OFFSET_BITS)
+
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <io.h>
+/* zlib.h uses off_t so we need to include it before we redefine off_t. */
+# include <zlib.h>
+
+// MSVC needs to call _stat64() instead of stat() and the struct which holds
+// the information is "struct _stat64" instead of "struct stat" so we just
+// use #define to replace both in one go.  We also want to use _fstat64()
+// instead of fstat() but in this case we can use a function-like macro.
+//
+// Mingw has _stat64() but unhelpfully for our purposes, it has an extra
+// underscore prefix on the struct name ("struct __stat64") so there we
+// target _stati64 instead.  This has 64-bit file size but the time_t size
+// is switchable.  We don't really care about file timestamps in the
+// library, so this should be fine.
+//
+// This hack is a problem is we ever want a method called "stat", or one called
+// fstat which takes 2 parameters, but we can probably live with these
+// limitations.
+
+# ifdef stat
+/* #  undef stat */
+# endif
+
+# ifdef fstat
+/* #  undef fstat */
+# endif
+
+# ifdef _MSC_VER
+#  define stat _stat64
+#  define fstat(FD, BUF) _fstat64(FD, BUF)
+# else
+#  define stat _stati64
+#  define fstat(FD, BUF) _fstati64(FD, BUF)
+# endif
+
+# ifdef lseek
+/* #  undef lseek */
+# endif
+
+# ifdef off_t
+/* #  undef off_t */
+# endif
+
+# define lseek(FD, OFF, WHENCE) _lseeki64(FD, OFF, WHENCE)
+/* Redefine via a typedef so C++ code off_t(-1) works - it wouldn't if we did:
+ * #define off_t long long
+ */
+typedef long long off_t_redefinition_typedef;
+# define off_t off_t_redefinition_typedef
+
+#endif
+
+/* MSVC defines _WIN32 but not __WIN32__. */
+#if !defined __WIN32__ && defined _WIN32
+# define __WIN32__
+#endif
+
+/* MSVC defines _WIN64 but not __WIN64__. */
+#if !defined __WIN64__ && defined _WIN64
+# define __WIN64__
+#endif
+
+/* _FORTIFY_SOURCE is only supported by GCC >= 4.1 and glibc >= 2.3.4, but it
+ * shouldn't cause a problem to define it where it's not supported and some
+ * distros may have backported support, so hardcoding version checks is
+ * counter-productive.
+ *
+ * Check if _FORTIFY_SOURCE is already defined to allow the user to override
+ * our choice with "./configure CPPFLAGS=-D_FORTIFY_SOURCE=0" or "...=1".
+ */
+#if defined __GNUC__ && !defined _FORTIFY_SOURCE
+# define _FORTIFY_SOURCE 2
+#endif
+
+/* For compilers which support it (such as GCC, clang, Intel's C++ compiler)
+ * we can use __builtin_expect to give the compiler hints about branch
+ * prediction.  See HACKING for how to use these.
+ */
+#if HAVE_DECL___BUILTIN_EXPECT
+/* The arguments of __builtin_expect() are both long, so use !! to ensure that
+ * the first argument is always an integer expression, and always 0 or 1, but
+ * still has the same truth value for the if or while it is used in.
+ */
+# define rare(COND) __builtin_expect(!!(COND), 0)
+# define usual(COND) __builtin_expect(!!(COND), 1)
+#else
+# define rare(COND) (COND)
+# define usual(COND) (COND)
+#endif
+
+/* Signal we're building the library so it's OK to include headers such as
+ * xapian/query.h directly.
+ */
+#define XAPIAN_LIB_BUILD 1
+
+/* With Sun CC 5.13 (Studio 12.4) on Solaris 11.2, <math.h> seems to get
+ * implicitly included somehow before <cmath>, and compilation fails due
+ * to 'std::exception' colliding with 'struct exception'.  It's not clear
+ * how to avoid this, so just define the same magic macro which <cmath> does
+ * before it includes <math.h>.
+ */
+#ifdef __SUNPRO_CC
+# define __MATHERR_RENAME_EXCEPTION
+#endif
+

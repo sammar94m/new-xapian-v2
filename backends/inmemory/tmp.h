05493ffc7871064da58560e9cd843044979f4664
Muhamad Sammar <muhamad.sammar@gmail.com>
2018-12-06 12:01:21 +0200

    adding keydomet files and database

---
+++ backends/inmemory/tmp.h
@@ -0,0 +1,21 @@
+/*
+ * tmp.h
+ *
+ *  Created on: Sep 18, 2018
+ *      Author: compm
+ */
+
+#ifndef BACKENDS_INMEMORY_TMP_H_
+#define BACKENDS_INMEMORY_TMP_H_
+
+
+#include "KeyDomet.h"
+#include <string>
+
+using namespace std;
+using namespace kdmt;
+
+typedef KeyDometStr<string, kdmt::KeyDometSize::SIZE_32BIT> kdmtStr;
+
+
+#endif /* BACKENDS_INMEMORY_TMP_H_ */
